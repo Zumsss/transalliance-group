afcf9c430026c4c41e7ec1a579c9141a7d8e5ed9
Izu <okolieizuchukwu@yahoo.com>
2024-06-27 11:59:47 +0100

    i added the footer images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import Frame527 from '../assets/images/Frame527.png';
 import Frame526 from '../assets/images/Frame526.png';
 import TravelConceptWithDocuments from '../assets/images/TravelConceptWithDocuments.png'; // The image with the passport and plane ticket
 import Frame525 from '../assets/images/Frame525.png'; // The "Discover More" button image
+import Frame5255 from '../assets/images/Frame5255.png'; // The footer image
+import Frame570 from '../assets/images/Frame570.png'; // The footer image
 
 function Home() {
 
@@ -13,7 +15,7 @@ function Home() {
   };
 
   return (
-    <div style={{ backgroundColor: '#122038' }}>
+    <div className="home-container">
       <section className="features">
         <div className="feature feature-left">
           <img src={Frame527} alt="get started" style={imageStyle} />
@@ -24,9 +26,34 @@ function Home() {
         </div>
       </section>
 
-      <section className="discover-more-section">
+      {/* <section className="discover-more-section">
         <img src={TravelConceptWithDocuments} alt="Travel Concept with Documents" className="travel-concept-img" />
         <img src={Frame525} alt="Discover More Button" className="discover-more-button-img" />
+      </section> */}
+
+      <section className="discover-more-section">
+        <img 
+          src={TravelConceptWithDocuments} 
+          alt="Travel Concept with Documents" 
+          className="travel-concept-image" 
+        />
+        <img 
+          src={Frame525} 
+          alt="Discover More" 
+          className="discover-more-button" 
+        /><br/>
+      </section>
+      <section>
+        <img 
+          src={Frame5255} 
+          alt="Discover More" 
+          className="footer-image" 
+        />
+        <img 
+          src={Frame570} 
+          alt="Discover More" 
+          className="footer-image" 
+        />
       </section>
 
     </div>
