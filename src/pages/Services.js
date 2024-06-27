c5810c39d479be8f9f9b0bff774724883406f8ce
Izu <okolieizuchukwu@yahoo.com>
2024-06-28 00:37:11 +0100

    Added 'Services' page's content

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,11 +1,41 @@
 // src/pages/Services.js
 import React from 'react';
+import Frame556 from '../assets/images/Frame556.png'; 
+import Frame598 from '../assets/images/Frame598.png'; 
+import Frame585 from '../assets/images/Frame585.png'; 
+
 
 function Services() {
   return (
-    <div>
-      <h2>Our Services</h2>
-      <p>Details of services offered.</p>
+    <div className='service-div'>
+      <div className="text-container">
+        <div className="text-left">
+          <h2>OUR SERVICE</h2>
+        </div>
+        <div className="text-right">
+          <h2>The Transalliance Group. difference</h2>
+          <p className='text-rightP'>Our global payments network is powered by passionate people, transparent customer relationships, and leading-edge technology.
+          </p>
+        </div>
+      </div>
+      <div className="text-container">
+        <img 
+          src={Frame556} 
+          alt="Travel Concept with Documents" 
+        />
+        <img 
+          src={Frame598} 
+          alt="Travel Concept with Documents"
+          className='Frame598'
+        />
+      </div>
+      <div>
+        <img 
+          src={Frame585} 
+          alt="service-footer"
+          className='Frame585'
+        />
+      </div>
     </div>
   );
 }
