b9c01dcfe4bd67fff3d77d8c9106d57e131d786d
Izu <okolieizuchukwu@yahoo.com>
2024-06-28 02:05:41 +0100

    I am done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,39 @@
 // src/pages/Contact.js
 import React from 'react';
+import image48 from '../assets/images/image48.png'; 
+import Frame591 from '../assets/images/Frame591.png'; 
+import Frame577 from '../assets/images/Frame577.png'; 
+import Frame558 from '../assets/images/Frame558.png'; 
 
 function Contact() {
   return (
     <div>
-      <h2>Contact Us</h2>
-      <p>Contact information.</p>
+      <div>
+        <img 
+          src={Frame558} 
+          alt="contact-header"
+          className='Frame558'
+        />
+      </div>
+      <div className="text-container">    
+        <img 
+          src={image48} 
+          alt="..."
+          className='image48'
+        />
+        <img 
+          src={Frame591} 
+          alt="..."
+          className='Frame591'
+        />
+      </div>
+      <div>
+        <img 
+          src={Frame577} 
+          alt="contact-footer"
+          className='Frame577'
+        />
+      </div>
     </div>
   );
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Frame556 from '../assets/images/Frame556.png'; 
 import Frame598 from '../assets/images/Frame598.png'; 
 import Frame585 from '../assets/images/Frame585.png'; 
+import FinancialServices from '../assets/images/FinancialServices.png';
+import IGaming from '../assets/images/IGaming.png';
+import NGOs from '../assets/images/NGOs.png';
+import Corporates from '../assets/images/Corporates.png';
 
 
 function Services() {
@@ -36,7 +40,36 @@ function Services() {
           className='Frame585'
         />
       </div>
+      <div className="grid-home">
+        <div className="grid-container">
+          <div className="grid-item">
+            <img src={FinancialServices} alt="Financial Services" />
+            <div className="overlay">
+              <div className="text">Financial Services<br/><span>Learn More</span></div>
+            </div>
+          </div>
+          <div className="grid-item">
+            <img src={IGaming} alt="iGaming" />
+            <div className="overlay">
+              <div className="text">iGaming<br/><span>Learn More</span></div>
+            </div>
+          </div>
+          <div className="grid-item">
+            <img src={NGOs} alt="NGOs" />
+            <div className="overlay">
+              <div className="text">NGOs<br/><span>Learn More</span></div>
+            </div>
+          </div>
+          <div className="grid-item">
+            <img src={Corporates} alt="Corporates" />
+            <div className="overlay">
+              <div className="text">Corporates<br/><span>Learn More</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
